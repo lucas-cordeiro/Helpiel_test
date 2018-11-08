c0333702a56b2533320441a11aa668059531f4f6
Lucas Cordeiro <lucas.cordeiro5@etec.sp.gov.br>
2018-11-08 18:35:25 -0200

    Add Bottom Navigation

---
+++ app/src/main/java/cordeiro/lucas/helpie/activity/MainActivity.java
@@ -0,0 +1,77 @@
+package cordeiro.lucas.helpie.activity;
+
+import android.support.annotation.NonNull;
+import android.support.design.bottomnavigation.LabelVisibilityMode;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import cordeiro.lucas.helpie.R;
+import cordeiro.lucas.helpie.fragment.PhotosFragment;
+import cordeiro.lucas.helpie.fragment.UsersFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configurar BottomNavigationView
+        configurarBottomNavigationView();
+    }
+
+    private void configurarBottomNavigationView() {
+
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottom_navigation);
+
+        //Configurações iniciais
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.setItemHorizontalTranslationEnabled(true);
+        bottomNavigationViewEx.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_SELECTED);
+        bottomNavigationViewEx.setTextVisibility(true);
+
+        //Habilitar navegação
+        habilitarNavagacao(bottomNavigationViewEx);
+        //Carregar Fragment Padrão
+        carregarFragmentPadrao();
+    }
+
+    private void habilitarNavagacao(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (menuItem.getItemId()){
+                    case R.id.ic_users:
+                        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right);
+                        fragmentTransaction.replace(R.id.viewPager, new UsersFragment());
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.ic_photos:
+                        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left);
+                        fragmentTransaction.replace(R.id.viewPager, new PhotosFragment());
+                        fragmentTransaction.commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void carregarFragmentPadrao() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right);
+        fragmentTransaction.replace(R.id.viewPager, new UsersFragment());
+        fragmentTransaction.commit();
+    }
+}
